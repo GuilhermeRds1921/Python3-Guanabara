2a9f989e5471c9d1169e42616d6d01455f289270
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-20 14:01:43 -0300

    Enunciado de 25 a 30

--- Exercises/028.py
+++ Exercises/028.py
@@ -1,8 +1,14 @@
+#        Jogo da Adivinhação
+# Escreva um programa que faça o computador "pensar" em um 
+# número inteiro entre 0 e 5 e peça para o usuário tentar descobrir
+# qual foi o número escolhido pelo computador.
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
 from random import randint
-b= randint(0,5)
-a= int(input("Escreva um Numero de 0 a 5: "))
+b = randint(0,5)
+a = int(input("Escreva um Numero de 0 a 5: "))
 if a==b:
-    print("Voce acertou ")
+    print("Você acertou ")
 else:
-    print("Voce errou ")
-print("O numero era {}".format(b))
+    print("Você errou ")
+print("O número era {}".format(b))
--- Exercises/029.py
+++ Exercises/029.py
@@ -1,7 +1,11 @@
-vel= int(input("Digite a velocidade do carro: "))
-if vel<=80:
-    print("Voce esta dentro do limite ")
+# Escreva um programa que leia a velocidade de um carro. 
+# Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi muldato. 
+# A multa vai custar R$7,00 para cada Km acima do limite.
+
+vel = float(input("Digite a velocidade do carro: "))
+if vel <= 80:
+    print("Você está dentro do limite ")
 else:
-    print("Voce esta acima do limite ")
-    vel=vel-80
-    print("Tera que pagar R${} de multa ".format(vel*7))
+    print("Você está acima do limite ")
+    vel = vel - 80
+    print("Terá que pagar R${:.2f} de multa ".format(vel * 7))
