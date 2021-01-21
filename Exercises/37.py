af757ecfe685855259de53554fcfb697db0e32f7
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-21 20:16:12 -0300

    Enunciado em Inglês 36 e 37

--- Exercises/37.py
+++ Exercises/37.py
@@ -1,3 +1,6 @@
+# Write a program that reads any integer and ask the 
+# user to choose the convention base.
+
 num = int(input('Write the number: '))
 print('1 - Binary')
 print('2 - Octal')
@@ -7,7 +10,7 @@ if option == 1:
     print('The Number in Binary: {}'.format(bin(num)[2:]))
 elif option == 2:
     print('The Number in Octal: {}'.format(oct(num)[2:]))
-elif option ==3:
+elif option == 3:
     print('The Number in Hex: {}'.format(hex(num)[2:]))
 else:
     print('Chose a Valid option !')
