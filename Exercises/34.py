fb7dbe20d574b1cd3886b23fcee2b0391fd5aca7
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-21 20:09:07 -0300

    Enunciado 31 a 35

--- Exercises/34.py
+++ Exercises/34.py
@@ -1,6 +1,11 @@
-sal= float(input("Digite o seu Salario: "))
-if sal>=1250:
-    sal=sal+(sal*0.1)
+# Escreva um programa que pergunte o salário de um funcionário e
+# calcule o valor do seu aumento. Para salários superiores a R$1.250,00, 
+# calcule um aumento de 10%. 
+# Para os inferiores ou iguais, o aumento é de 15%.
+
+sal = float(input("Digite o seu Salário: "))
+if sal >= 1250:
+    sal = sal + (sal * 0.1)
 else:
-    sal=sal+(sal*0.15)
-print("Seu novo salario e de {}".format(sal))
+    sal = sal + (sal * 0.15)
+print("Seu novo salário e de R${:.2f}".format(sal))
--- Exercises/35.py
+++ Exercises/35.py
@@ -1,19 +1,23 @@
-a= float(input("Digite a Primeira Reta: "))
-b= float(input("Digite a Segunda Reta: "))
-c= float(input("Digite a Terceira Reta: "))
+# Desenvolva um programa que leia o comprimento de
+# três retas e diga ao usuário se elas podem 
+# ou não formar um trângulo.
+
+a = float(input("Digite a Primeira Reta: "))
+b = float(input("Digite a Segunda Reta: "))
+c = float(input("Digite a Terceira Reta: "))
 if a < b:
     ax = a
     a = c
     c = ax
-if b<c:
+if b < c:
     ax = b
     b = c
-    c =ax
+    c = ax
 if a < b:
     ax = a
     a = c
     c = ax
-if a<b+c:
-    print("Pode formar um Triangulo ")
+if a < b + c:
+    print("Pode formar um Triângulo ")
 else:
-    print("Nao pode formar um Triangulo ")
+    print("Não pode formar um Triângulo ")
