97fb926063f3dd2e0cfd61b475177b0fb556c72e
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-21 20:26:33 -0300

    arrumando acentuação

--- Exercises/004.py
+++ Exercises/004.py
@@ -1,11 +1,11 @@
 # Faça um programa que leia algo pelo teclado e mostre na tela 
 # seu tipo primitivo e todas as infomações possíveis sobre ele.
 
-valor = input('Variavel: ')
-print('O tipo da variavel e: ',type(valor))
+valor = input('Variável: ')
+print('O tipo da variável e: ',type(valor))
 print('E feito de espacos: ', valor.isspace())
-print('E numerico: ', valor.isnumeric())
+print('E númerico: ', valor.isnumeric())
 print('E alfabetico: ', valor.isalpha())
-print('E tudo maiusculo: ', valor.isupper())
-print('E tudo minusculo: ', valor.islower())
+print('E tudo maiúsculo: ', valor.isupper())
+print('E tudo minúsculo: ', valor.islower())
 print('E capitalizada: ', valor.istitle())
