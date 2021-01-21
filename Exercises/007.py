a5fda0fc5a26bb93e801d975f823cf09da8612f4
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-21 20:27:46 -0300

    Acentuação

--- Exercises/007.py
+++ Exercises/007.py
@@ -4,4 +4,4 @@
 nota1 = float(input("Digite a Primera Nota: "))
 nota2 = float(input("Digite a Segunda Nota: "))
 media = float((nota1 + nota2) / 2)
-print("A media eh: {}".format(media))
+print("A média eh: {}".format(media))
