51792834a57ac39030596ebfc979497b76eccc1f
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-22 20:51:46 -0300

    Enunciado 50 em Inglês

--- Exercises/050.py
+++ Exercises/050.py
@@ -1,3 +1,7 @@
+# Develop a program that reads two integers and shows 
+# the sum of only those that are even. 
+# If the value entered is odd, discard it.
+
 pair = 0
 for i in range(1,7):
     num = int(input('Write the {}º number: '.format(i)))
