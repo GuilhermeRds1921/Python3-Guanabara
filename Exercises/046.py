d3c4a46a8d1a2dc6abb55b37dcf79084debcc0e2
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-22 20:45:30 -0300

    Enunciado 46 a 49 em Inglês

--- Exercises/046.py
+++ Exercises/046.py
@@ -1,7 +1,10 @@
+# Develop a program that shows on the screen a countdown
+# to the fireworks burst, going from 10 to 0, 
+# with 1 second delay between them.
 from time import sleep
-print('=' * 17)
+print('=' * 25)
 print('|\tcountdown\t|')
-print('=' * 17)
+print('=' * 25)
 sec = 10
 for i in range(0, 11):
     print(sec)
