2a5c5bfb39e152e887856b76541de1067b6b1664
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-24 21:54:16 -0300

    52

---
+++ Exercises/52.py
@@ -0,0 +1,9 @@
+num = int(input('Write a number: '))
+a = 0
+for i in range(2,num):
+    if num%i ==0:
+        a += 1
+if a == 0:
+    print('{} is prime. '.format(num))
+else:
+    print('{} isnt prime. '.format(num))
