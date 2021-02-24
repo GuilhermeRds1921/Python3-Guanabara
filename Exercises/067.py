11b6305e22c39c15930398d96c43db5a743444e6
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-02-24 11:30:38 -0300

    Sintaxe

--- Exercises/067.py
+++ Exercises/067.py
@@ -1,10 +1,10 @@
 # Develop a program that shows the multiplication table of several numbers, 
 # one at a time, for each value entered by the user. 
-# The program will be interrupted when the requested number is negative.
+# The program will be interrupted when the requested number is negative or Zero.
 
 while True:
-    num = int(input('\nChoose a Number to see the multiplication table(>0): '))
-    print('='*20)
+    num = int(input('\nChoose a Number to see the multiplication table( >0 ): '))
+    print('=' * 20)
     if num <= 0:
         break
     for i in range(1,11):
